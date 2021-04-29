1a4ac48733d832d6dc4c8f74fafd7af4c3ce675e
Hong Shuning <hongshuning@gmail.com>
2021-04-29 09:21:33 +0800

    Support alternative servers

--- etherscan_py/etherscan_py.py
+++ etherscan_py/etherscan_py.py
@@ -65,12 +65,13 @@ def chunks(start, end, n):
     return list(map(lambda x: (x[0], x[-1]),res))
 
 class Client:
-    def __init__(self, api_key):
+    def __init__(self, url, api_key):
+        self.url = url
         self.api_key = api_key
         self.session = requests.Session()
     
     def get(self, module, action, extra_data=""):
-        url = f"http://api.etherscan.io/api?module={module}&action={action}&{extra_data}&apikey={self.api_key}"
+        url = f"{self.url}/api?module={module}&action={action}&{extra_data}&apikey={self.api_key}"
         r = self.session.get(url)
         if r.status_code == 200:
             res = r.json()
